97fd283ddaab48d122acb6c6ffe29176cbe6ff5d
louispCx <99268939+louispCx@users.noreply.github.com>
2022-12-03 03:19:01 +0900

    Update pullrequest_comment.py

--- pullrequest_comment.py
+++ pullrequest_comment.py
@@ -4,7 +4,10 @@ with open('cx_summaryConsole.txt') as f:
     title='![Logo](https://user-images.githubusercontent.com/42570337/152954017-2101d077-2be7-48a8-8d34-618fe5a7c779.png)\n'
     output=title
     for s in html_string:
-        output += "#####"+s
+        if 'Summary' in s:
+            output += "#####"+s
+        else:
+            output += s
     dictionary = {
         "body": output
     }
