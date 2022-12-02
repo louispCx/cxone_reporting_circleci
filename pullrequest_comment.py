dbf96878f1413bb04ded79e73c432e72f4b7c512
louispCx <99268939+louispCx@users.noreply.github.com>
2022-12-02 20:40:52 +0900

    Update pullrequest_comment.py

--- pullrequest_comment.py
+++ pullrequest_comment.py
@@ -1,9 +1,12 @@
 import json
 with open('cx_summaryConsole.txt') as f:
-    html_string=f.read()
+    html_string=f.readlines()
     title='![Logo](https://user-images.githubusercontent.com/42570337/152954017-2101d077-2be7-48a8-8d34-618fe5a7c779.png)\n'
+    output=title
+    for s in html_string:
+        output =+ "###"+s
     dictionary = {
-        "body": title+html_string
+        "body": output
     }
     json_object = json.dumps(dictionary, indent=4)
     with open("cx_github_pullrequest_comment.json", "w") as outfile:
