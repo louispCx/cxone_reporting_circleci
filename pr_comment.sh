0aca0feed72fab596220c7c5a9c52c46472e3f95
louispCx <99268939+louispCx@users.noreply.github.com>
2022-12-03 04:46:32 +0900

    Update pr_comment.sh

--- pr_comment.sh
+++ pr_comment.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
-python --version
 sudo apt-get install jq
-
-rm -rf cxone_reporting_circleci
-git clone https://github.com/louispCx/cxone_reporting_circleci.git
-export CX_SCANID=$(python cxone_reporting_circleci/get_scanID_from_file.py)
-../cx \
-results show --scan-id=$CX_SCANID \
---base-uri $CX_BASE_URI \
---base-auth-uri $CX_BASE_AUTH_URI \
---tenant $CX_TENANT \
---apikey $CX_API_KEY \
---report-format summaryConsole > cx_summaryConsole.txt
-cat cx_summaryConsole.txt
-python cxone_reporting_circleci/pullrequest_comment.py
 pr_response=$(curl --location --request GET "https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/pulls?head=$CIRCLE_PROJECT_USERNAME:$CIRCLE_BRANCH&state=open" \
 -u $GH_USER:$GH_TOKEN)
 #echo "pr_response:"
@@ -21,6 +7,18 @@ pr_response=$(curl --location --request GET "https://api.github.com/repos/$CIRCL
 if [ $(echo $pr_response | jq length) -eq 0 ]; then
   echo "No PR found to update"
 else
+  python --version
+  export CX_SCANID=$(python cxone_reporting_circleci/get_scanID_from_file.py)
+  ../cx \
+  results show --scan-id=$CX_SCANID \
+  --base-uri $CX_BASE_URI \
+  --base-auth-uri $CX_BASE_AUTH_URI \
+  --tenant $CX_TENANT \
+  --apikey $CX_API_KEY \
+  --report-format summaryConsole > cx_summaryConsole.txt
+  cat cx_summaryConsole.txt
+  python cxone_reporting_circleci/pullrequest_comment.py
+
   pr_comment_url=$(echo $pr_response | jq -r ".[]._links.comments.href")
   curl --location --request POST "$pr_comment_url" \
   -u $GH_USER:$GH_TOKEN \
