9ede0ac8fc4737c2a206a47197d23d522b436247
louispCx <99268939+louispCx@users.noreply.github.com>
2022-12-02 19:38:55 +0900

    Create pullrequest_comment.py

---
+++ pullrequest_comment.py
@@ -0,0 +1,10 @@
+import json
+with open('cx_summaryConsole.txt') as f:
+    html_string=f.read()
+    title='![Logo](https://user-images.githubusercontent.com/42570337/152954017-2101d077-2be7-48a8-8d34-618fe5a7c779.png)\n'
+    dictionary = {
+        "body": title+html_string
+    }
+    json_object = json.dumps(dictionary, indent=4)
+    with open("cx_github_pullrequest_comment.json", "w") as outfile:
+        outfile.write(json_object)
