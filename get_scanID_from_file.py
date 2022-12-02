73019fe901bf95f16e32d9a0984953912ec15874
louispCx <99268939+louispCx@users.noreply.github.com>
2022-12-02 17:53:57 +0900

    Update get_scanID_from_file.py

--- get_scanID_from_file.py
+++ get_scanID_from_file.py
@@ -1,6 +1,6 @@
 import json
 
-with open('./cx_result.json', encoding='utf-8') as f:
+with open('../cx_result.json', encoding='utf-8') as f:
     data=json.load(f)
 for d in data:
     if d=='scanID' :
