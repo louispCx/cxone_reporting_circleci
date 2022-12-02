521a06eb9d0aee05a8340703672161d1a2875a64
louispCx <99268939+louispCx@users.noreply.github.com>
2022-12-03 04:53:43 +0900

    Update pullrequest_comment.py

--- pullrequest_comment.py
+++ pullrequest_comment.py
@@ -1,7 +1,7 @@
 import json
 with open('cx_summaryConsole.txt') as f:
     html_string=f.readlines()
-    title='![checkmarx_circleci](https://user-images.githubusercontent.com/99268939/205370786-3aa37659-b5d3-4a44-a48c-ae501470a729.png)'
+    title='![checkmarx_circleci](https://user-images.githubusercontent.com/99268939/205370786-3aa37659-b5d3-4a44-a48c-ae501470a729.png)\n'
     output=title
     for s in html_string:
         if 'Details' in s:
